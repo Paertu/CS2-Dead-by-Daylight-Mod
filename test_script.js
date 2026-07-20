397c7646c4b9ddf661514e35bb69166914db3cc3
Emi <thomashenrypaert@gmail.com>
2026-07-20 17:17:12 +0300

    Worked on Generator Functionality
    -Generators now restart after rounds
    -Generators are now separate from each other

--- test_script.js
+++ test_script.js
@@ -1,29 +1,66 @@
 import { Instance } from "cs_script/point_script";
-Instance.ServerCommand("sv_cheats 1");
-Instance.ServerCommand("mp_do_warmup_period 0");
-Instance.ServerCommand("mp_warmuptime 0");
-Instance.ServerCommand("mp_freezetime 0");
-Instance.ServerCommand("mp_buytime 10000");
+let DEBUG_MODE = false;
+
+Instance.OnActivate(() => {
+    Instance.ServerCommand("sv_cheats 1");
+    Instance.ServerCommand("mp_do_warmup_period 0");
+    Instance.ServerCommand("mp_warmuptime 0");
+    Instance.ServerCommand("mp_freezetime 0");
+    Instance.ServerCommand("mp_buytime 10000");
+});
 
 const repairGoal = 100;
 let repairProgress = 0;
-let is_running = false;
-let is_special = false;
+let generators = {};
 
 Instance.Msg(Object.getOwnPropertyNames(Instance));
 
-Instance.OnScriptInput("RepairAction", (ctx) => {
-    let gennyName = ctx.caller.GetEntityName();
-    Instance.Msg(`caller expanded:${Object.getOwnPropertyNames(ctx.caller)}`);   
-    Instance.Msg(`genny name:${JSON.stringify(gennyName)}`);   
+generatorStatusCleanup();
 
-    if (!is_running) {
-        repairProgress = repairProgress + 25;
-        Instance.Msg(`ctx stringified:${JSON.stringify(ctx)} Progress: ${repairProgress}`);   
+Instance.OnScriptInput("RepairAction", (ctx) => {
+    if (DEBUG_MODE) {
+        Instance.Msg(`ctx stringified:${JSON.stringify(ctx)}`);   
+        Instance.Msg(`caller expanded:${Object.getOwnPropertyNames(ctx.caller)}`);   
+        Instance.Msg(`activator expanded:${Object.getOwnPropertyNames(ctx.activator)}`);   
     }
-    Instance.Msg(`ctx stringified:${JSON.stringify(ctx)} Progress: ${repairProgress}`);
-    if (repairGoal <= repairProgress) {
-        is_running = true;
-        Instance.Msg(`Generator is running ${is_running}`);
+    let gennyName = ctx.caller.GetEntityName();
+    if (generators[gennyName] === undefined) {
+        Instance.Msg(`genny was undefined, created one called:${JSON.stringify(gennyName)}`);   
+        generators[gennyName] = {
+            progress: 0,
+            goal: repairGoal,
+            isRunning: false,
+        }
+    }
+
+    let generator = generators[gennyName]
+    if (generator.goal > generator.progress) {
+        generator.progress += 5;
+        Instance.Msg(`Progress: ${generator.progress}`);   
     }
 });
+
+function generatorStatusCleanup() {
+    Instance.Msg("Starting Generator Status Cleanup");
+    try {
+        Instance.OnBeginRoundRestart(() => {
+            try {
+                Instance.Msg("Attempting to reset gens");
+                let allGenerators = Object.values(generators);
+                Instance.Msg(`Attempting to reset ${allGenerators.length} gens`);
+
+                allGenerators.forEach(gen => {
+                    gen.progress = 0;
+                    gen.isRunning = false;
+                });
+            }
+            catch (error) {
+                Instance.Msg(`CLEANUP ERROR: ${error}`);
+            }
+            Instance.Msg("Reset generators");
+        });
+    }
+    catch (error) {
+        Instance.Msg(`CLEANUP FUNCTION ERROR: ${error}`);
+    }
+}
